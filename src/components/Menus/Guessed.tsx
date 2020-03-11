ea4ef8bc10c7cfb2ccc7c2e6917faa00821787a0
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-03-11 22:52:08 +0200

    add confetti to guess screen

--- src/components/Menus/Guessed.tsx
+++ src/components/Menus/Guessed.tsx
@@ -2,23 +2,42 @@ import * as React from "react";
 import styled from "styled-components";
 import { styled as style } from "@material-ui/core/styles";
 import { Button, GuessedIcon, Main } from "components";
+import confetti from "canvas-confetti";
+
+const leftRightConfetti = () => {
+  confetti({
+    particleCount: 100,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+  });
+  confetti({
+    particleCount: 100,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+  });
+};
 
 export interface GuessedProps {
   word: string;
   getNewWord: () => void;
   score: number;
 }
-export const Guessed: React.FC<GuessedProps> = ({ word, getNewWord, score }) => (
-  <Card>
-    <h1>Well done!</h1>
-    <p>The correct word was {word.toUpperCase()}</p>
-    <GuessedIcon />
-    <p className="score">Current score: {score}</p>
-    <ResetButton autoFocus onClick={getNewWord}>
-      Next round
-    </ResetButton>
-  </Card>
-);
+export const Guessed: React.FC<GuessedProps> = ({ word, getNewWord, score }) => {
+  React.useEffect(() => leftRightConfetti(), []);
+  return (
+    <Card>
+      <h1>Well done!</h1>
+      <p>The word was {word.toUpperCase()}</p>
+      <GuessedIcon />
+      <p className="score">Current score: {score}</p>
+      <ResetButton autoFocus onClick={getNewWord}>
+        Next round
+      </ResetButton>
+    </Card>
+  );
+};
 
 const Card = styled(Main)`
   background-color: #28a745;
