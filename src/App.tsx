f146a72e087303d91505cb6ab59bf0d96c3a8b41
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-03-11 18:00:53 +0200

    add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
+import styled from "styled-components";
 import { Background, Game, Intro } from "components";
+
+import gitGubIcon from "components/Icons/gitGubIcon.svg";
 
 function App() {
   const [page, setPage] = React.useState<IPages>("Intro");
   return (
     <Background>
+      <Link href="https://github.com/snelsi/hangman" target="_blank" rel="noopener">
+        <img src={gitGubIcon} alt="gitHub" />
+      </Link>
       <Pages page={page} setPage={setPage} />
     </Background>
   );
@@ -21,3 +27,36 @@ const Pages: React.FC<PagesProps> = ({ page, setPage }) => {
   throw new Error("");
 };
 export default App;
+
+const Link = styled.a`
+  --size: 24px;
+  border-radius: 50%;
+  position: fixed;
+  height: var(--size);
+  width: var(--size);
+  top: var(--size);
+  right: var(--size);
+  opacity: 0.3;
+  outline: none;
+
+  transition: all 0.15s ease;
+
+  &:hover,
+  &:focus {
+    opacity: 1;
+    transform: scale(1.05);
+  }
+
+  &:focus {
+    box-shadow: 0 0 1px 2px #4589ff;
+  }
+  &:active {
+    opacity: 0.6;
+    transform: scale(0.95);
+  }
+  & > img {
+    display: block;
+    height: 100%;
+    width: 100%;
+  }
+`;
